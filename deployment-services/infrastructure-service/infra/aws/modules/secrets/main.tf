04578cee03957238a8d4038dc71c731bff536a65
MohamedAklamaash <aklamaash78@gmail.com>
2026-03-08 09:16:32 +0530

    fixed role in main.tf

--- deployment-services/infrastructure-service/infra/aws/modules/secrets/main.tf
+++ deployment-services/infrastructure-service/infra/aws/modules/secrets/main.tf
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "secrets_kms_policy" {
     effect = "Allow"
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/DeploymentRole"]
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LaunchpadDeploymentRole"]
     }
     actions = [
       "kms:Encrypt",
